e17b97ebbefede59ef20e20f5283a389b684aae1
A233S <71011762+A233S@users.noreply.github.com>
2023-09-04 13:51:32 +0800

    Update Apis.kt

--- app/src/main/java/com/nohjunh/test/network/Apis.kt
+++ app/src/main/java/com/nohjunh/test/network/Apis.kt
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 interface Apis {
     @Headers(
         "Content-Type:application/json",
-        "Authorization:Bearer API")
+        "Authorization:Bearer sk-2r9mINIaay7KW7HF76B29b36079b48BcA2DbCaD58530A5Bd")
     @POST("v1/completions")
     suspend fun postRequest(
         @Body json : JsonObject
